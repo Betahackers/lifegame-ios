818605d76198c0a5e07b2df9b21e5bd49e981b7c
Duncan Campbell <duncan.a.campbell@gmail.com>
2017-02-19 20:26:26 +0100

    GAME END

--- lifegame/lifegame/LifegameViewController.swift
+++ lifegame/lifegame/LifegameViewController.swift
@@ -116,6 +116,8 @@ class LifegameViewController: UIViewController, KolodaViewDataSource, KolodaView
     // MARK: - View controller life cycle
     
     override func viewDidLoad() {
+        
+        yearOfLifeLabel.text = "15"
         loadDeck()
     }
     
